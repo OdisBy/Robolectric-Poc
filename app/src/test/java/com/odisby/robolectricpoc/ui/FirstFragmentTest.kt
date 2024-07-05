a51a560031126035746e45f37c95aee97bc6797b
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-07-05 10:16:30 -0300

    Improved test logging and reporting
    Added detailed test logging with lifecycle events, exceptions, and stack traces.
    Included a summary of test results (passed, failed,
     skipped) after test execution.
    Added a list of failed and skipped tests to the test report.

--- app/src/test/java/com/odisby/robolectricpoc/ui/FirstFragmentTest.kt
+++ app/src/test/java/com/odisby/robolectricpoc/ui/FirstFragmentTest.kt
@@ -6,7 +6,6 @@ import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
